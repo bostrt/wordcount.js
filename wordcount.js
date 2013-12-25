6c521fcdc69a20e4b14deb138f6d1e898e018296
Robert Bost <bostrt@gmail.com>
2013-12-25 10:57:31 -0500

    more refactoring of comments

--- wordcount.js
+++ wordcount.js
@@ -1,12 +1,23 @@
 (function(root){
     /**
-     * WordCount.js is a client-side JS library that counts bytes, words, lines 
-     * chars, and calculates the longest line of a file. This library utilized
-     * the HTML5 File API.
+     * @module WordCount
+     */
+
+    /**
      * @title WordCount.js
      * @author Robert Bost <bostrt at gmail dot com>
      * @copyright &copy; 2013 Robert Bost
      * @license MIT
+     * WordCount.js is a client-side JS library that counts bytes, words, lines 
+     * chars, and calculates the longest line of a file. This library utilized
+     * the HTML5 File API. 
+     *
+     * > # Callbacks
+     * Callbacks are passed into each of WordCount's 
+     * functions. Each callback should accept 3 parameters, (1) the value that the 
+     * function is calculating or finding, (2) the File object that was uploaded, 
+     * (3) and the ID selector of the file input element.
+     * `callback(value, file, inputSelector)`
      */
     var WordCount = function() {
     };
@@ -15,17 +26,8 @@
      * When a file is uploaded using the file input element with the given input
      * selector then the number of the bytes in the uploaded file is calculated
      * and then callback is called.
-     * 
      * @param {String} inputSelector - The selector for a file input element.
-     * @param {Function} callback - Called when a file has been completely uploaded
-     * and bytes have been calculated. The callback should take three parameters:
-     * 
-     * <pre>
-     * callback(size, file, inputSelector)
-     * <b>size</b> - The file size in bytes
-     * <b>file</b> - The file that was uploaded
-     * <b>inputSelector</b> - The original input selector for the file input element being monitored.
-     * </pre>
+     * @param {Function} callback - Called when a file has been completely uploaded and bytes have been calculated.
      * @returns {undefined}
      */
     WordCount.bytes = function(inputSelector, callback){
@@ -40,17 +42,7 @@
      * is called.
      * 
      * @param {String} inputSelector - The selector for a file input element.
-     * @param {Function} callback - Called when a file has been completely uploaded
-     * and the longest line has been found (if any lines). A callback should
-     * take three parameters:
-     * 
-     * <pre>
-     * callback(longestLine, file, inputSelector)
-     * <b>longestLine</b> - The longest line in the file
-     * <b>file</b> - The file that was uploaded
-     * <b>inputSelector</b> - The original input selector for the file
-     * input element being monitored.
-     * </pre>
+     * @param {Function} callback - Called when a file has been completely uploaded and the longest line has been found.
      * @returns {undefined}
      */
     WordCount.longest = function(inputSelector, callback) {
@@ -101,15 +93,7 @@
      * callback is called. A word is a non-zero-length sequence of characters
      * delimited by white space.
      * @param {String} inputSelector - The selector for a file input element.
-     * @param {Function} callback - Called after a file is available and the number
-     * of words have been calculated. A callback should take three parameters:
-     * <pre>
-     * callback(words, file, inputSelector)
-     * <b>words</b> - The number of words in the file
-     * <b>file</b> - The file that was uploaded
-     * <b>inputSelector</b> - The original input selector for the file
-     * input element being monitored.
-     * </pre>
+     * @param {Function} callback - Called after a file is available and the number of words have been calculated.
      * @returns {undefined}
      */
     WordCount.words = function(inputSelector, callback) {
@@ -155,16 +139,7 @@
      * selector then the number of chars in the file are calculated and the
      * callback is called.
      * @param {String} inputSelector - The selector for a file input element.
-     * @param {Function} callback - Called after a file is available and the
-     * number of chars in that file have been calculated. A callback should
-     * take three parameters:
-     * <pre>
-     * callback(chars, file, inputSelector)
-     * <b>chars</b> - The number of chars in the file
-     * <b>file</b> - The file that was uploaded
-     * <b>inputSelector</b> - The original input selector for the file
-     * input element being monitored.
-     * </pre>
+     * @param {Function} callback - Called after a file is available and the number of chars in that file have been calculated.
      * @returns {undefined}
      */    
     WordCount.chars = function(inputSelector, callback) {
@@ -198,16 +173,7 @@
      * selector then the number of lines in the file are calculated and the
      * callback is called.
      * @param {String} inputSelector - The selector for a file input element.
-     * @param {Function} callback - Called after a file is available and the
-     * number of lines in that file have been calculated. A callback should
-     * take three parameters:
-     * <pre>
-     * callback(lines, file, inputSelector)
-     * <b>lines</b> - The number of lines in the file
-     * <b>file</b> - The file that was uploaded
-     * <b>inputSelector</b> - The original input selector for the file
-     * input element being monitored.
-     * </pre>
+     * @param {Function} callback - Called after a file is available and the number of lines in that file have been calculated.
      * @returns {undefined}
      */
     WordCount.lines = function(inputSelector, callback) {
