8d4a7f64e029012a19a90b2d17374dd26246986f
Robert Bost <bostrt@gmail.com>
2013-12-31 23:37:04 -0500

    Updated param name.

--- wordcount.js
+++ wordcount.js
@@ -17,7 +17,7 @@
      * functions. Each callback should accept 3 parameters, (1) the value that the 
      * function is calculating or finding, (2) the File object that was uploaded, 
      * (3) and the ID selector of the file input element.
-     * `callback(value, file, inputSelector)`
+     * `callback(value, file, selector)`
      */
     var WordCount = function() {
     };
@@ -26,13 +26,13 @@
      * When a file is uploaded using the file input element with the given input
      * selector then the number of the bytes in the uploaded file is calculated
      * and then callback is called.
-     * @param {String} inputSelector - The selector for a file input element.
+     * @param {String} selector - The ID selector for a file input element.
      * @param {Function} callback - Called when a file has been completely uploaded and bytes have been calculated.
      * @returns {undefined}
      */
-    WordCount.bytes = function(inputSelector, callback){
-        addFileChangeListener(inputSelector, function(file) {
-            callback(file.size, file, inputSelector);           
+    WordCount.bytes = function(selector, callback){
+        addFileChangeListener(selector, function(file) {
+            callback(file.size, file, selector);
         });
     };
 
@@ -41,17 +41,17 @@
      * selector then the longest line in the file is located and the callback
      * is called.
      * 
-     * @param {String} inputSelector - The selector for a file input element.
+     * @param {String} selector - The ID selector for a file input element.
      * @param {Function} callback - Called when a file has been completely uploaded and the longest line has been found.
      * @returns {undefined}
      */
-    WordCount.longest = function(inputSelector, callback) {
-        addFileChangeListener(inputSelector, function(file) {
+    WordCount.longest = function(selector, callback) {
+        addFileChangeListener(selector, function(file) {
             var reader = new FileReader();
             reader.onload = function(e) {
                 if (e.target.readyState === 2) {
                     var text = e.target.result;
-                    callback(WordCount.longestInText(text) ,file, inputSelector);
+                    callback(WordCount.longestInText(text) ,file, selector);
                 }
             };
             
@@ -92,17 +92,17 @@
      * selector then the number of words in the file is calculated and the
      * callback is called. A word is a non-zero-length sequence of characters
      * delimited by white space.
-     * @param {String} inputSelector - The selector for a file input element.
+     * @param {String} selector - The ID selector for a file input element.
      * @param {Function} callback - Called after a file is available and the number of words have been calculated.
      * @returns {undefined}
      */
-    WordCount.words = function(inputSelector, callback) {
-        addFileChangeListener(inputSelector, function(file) {
+    WordCount.words = function(selector, callback) {
+        addFileChangeListener(selector, function(file) {
            var reader = new FileReader() ;
            reader.onload = function(e) {
              if (e.target.readyState === 2) {
                  var text = e.target.result;
-                 callback(WordCount.wordsInText(text), file, inputSelector);
+                 callback(WordCount.wordsInText(text), file, selector);
              }  
            };
            
@@ -138,18 +138,18 @@
      * When a file is uploaded using the file input element with the given input
      * selector then the number of chars in the file are calculated and the
      * callback is called.
-     * @param {String} inputSelector - The selector for a file input element.
+     * @param {String} selector - The ID selector for a file input element.
      * @param {Function} callback - Called after a file is available and the number of chars in that file have been calculated.
      * @returns {undefined}
      */    
-    WordCount.chars = function(inputSelector, callback) {
-        addFileChangeListener(inputSelector, function(file) {
+    WordCount.chars = function(selector, callback) {
+        addFileChangeListener(selector, function(file) {
             var reader = new FileReader();
             reader.onload = function(e) {
                 if (e.target.readyState === 2) {
                     var text = e.target.result;
                     var charsLength = WordCount.charsInText(text);
-                    callback(charsLength, file, inputSelector);
+                    callback(charsLength, file, selector);
                 }
             };
             
@@ -172,12 +172,12 @@
      * When a file is uploaded using the file input element with the given input
      * selector then the number of lines in the file are calculated and the
      * callback is called.
-     * @param {String} inputSelector - The selector for a file input element.
+     * @param {String} selector - The ID selector for a file input element.
      * @param {Function} callback - Called after a file is available and the number of lines in that file have been calculated.
      * @returns {undefined}
      */
-    WordCount.lines = function(inputSelector, callback) {
-        addFileChangeListener(inputSelector, function(file) {
+    WordCount.lines = function(selector, callback) {
+        addFileChangeListener(selector, function(file) {
             var reader = new FileReader();
             reader.onload = function(e) {
                 if (e.target.readyState === 2) {
@@ -185,7 +185,7 @@
                     if (e.loaded > 0) {
                         // File has contents
                         var string = e.target.result;
-                        callback(WordCount.linesInText(string), file, inputSelector);
+                        callback(WordCount.linesInText(string), file, selector);
                     }
                 }
             };
@@ -214,8 +214,8 @@
      * When the "change" event is fired, the callback is called for each
      * file that was supplied.
      */
-    function addFileChangeListener(inputSelector, callback) {
-        var input = document.getElementById(inputSelector);
+    function addFileChangeListener(selector, callback) {
+        var input = document.getElementById(selector);
         input.addEventListener("change", function(event) {
             var files = event.target.files;
             if (files != undefined) {
